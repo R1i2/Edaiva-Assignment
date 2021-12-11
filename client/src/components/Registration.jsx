54cf3ad284a5262f3b92415a0bca6bf91b19b27c
Rishav Raj <52792420+R1i2@users.noreply.github.com>
2021-12-11 16:19:06 +0530

    source inside client

---
+++ client/src/components/Registration.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "./Form";
+import Footer from "./Footer";
+import Navigation from "./Navigation";
+function Registration()
+{
+    return(
+        <div className="Registration">
+            <Navigation />
+            <Form />
+            <Footer />
+        </div>
+    )
+}
+export default Registration();
